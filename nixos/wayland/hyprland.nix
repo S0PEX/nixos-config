1450522f9234d1329f0b3a8fb0df70ed2e0e5ce9
Artur Komaristych <artur.komaristych@gmail.com>
2024-10-14 20:21:50 +0200

    refactor(hyprland): enable XDG portal support in Hyprland configuration

--- nixos/wayland/hyprland.nix
+++ nixos/wayland/hyprland.nix
@@ -7,6 +7,13 @@
     xwayland.enable = true;
   };
 
+  # Enable XDG portal support
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = with pkgs; [
+    xdg-desktop-portal-hyprland
+    xdg-desktop-portal-gtk
+  ];
+
   # Add default packages
   environment.systemPackages = with pkgs; [
     waybar # Status bar for Sway
@@ -19,6 +26,10 @@
   environment.sessionVariables = {
     # If cursor becomes invisible
     WLR_NO_HARDWARE_CURSORS = "1";
+
+    # Enable XDG portal support
+    NIXOS_XDG_OPEN_USE_PORTAL = "1";
+    MOZ_ENABLE_WAYLAND = "1";
 
     # Hint electron apps to use wayland
     NIXOS_OZONE_WL = "1";
