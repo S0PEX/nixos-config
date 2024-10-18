ba3abae7f25761d03f3c13436223647870eba511
Artur Komaristych <artur.komaristych@gmail.com>
2024-10-18 09:44:52 +0200

    feat(s0pex): add zed-editor and texliveFull packages

--- users/s0pex/home-manager.nix
+++ users/s0pex/home-manager.nix
@@ -1,13 +1,16 @@
 { inputs, ... }:
 
-{ config, pkgs, ... }:
+{ config, pkgs, pkgs-unstable, ... }:
 
 {
   # Home-manager 22.11 requires this be set. We never set it so we have
   # to use the old state version.
   home.stateVersion = "24.05";
 
+  # User packages
   home.packages = with pkgs; [
+    pkgs-unstable.zed-editor
+
     # Terminal additions
     oh-my-fish
 
@@ -17,18 +20,26 @@
     # Communications
     discord # Discord chat client
     ferdium # Multi service chat client
-    # Git clients
-    gitkraken
+
+    # Writing tools
+    texliveFull # Full TeX Live distribution
 
     # IDEs
     vscode # Visual Studio Code
-    #jetbrains.goland         # GoLand IDE
-    #jetbrains.idea-ultimate  # IntelliJ IDEA
-    #jetbrains.pycharm-professional        # PyCharm
-    #jetbrains.datagrip       # DataGrip
+    jetbrains.goland # GoLand IDE
+    jetbrains.idea-ultimate # IntelliJ IDEA
+    jetbrains.pycharm-professional # PyCharm
+    jetbrains.datagrip # DataGrip
 
     # Formatter for Nix
     nixpkgs-fmt
+
+    # Programming languages
+    go # Go programming language
+    nodejs_20 # Node.js
+
+    # Git clients
+    gitkraken
   ];
 
   # Theme settings
