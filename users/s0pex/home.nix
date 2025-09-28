deef97845249d6cc98eb98ab8313a7ca530c3400
Artur Komaristych <artur.komaristych@gmail.com>
2025-09-28 11:29:38 +0200

    refactor(s0pex): update home.packages to use pkgs-unstable

--- users/s0pex/home.nix
+++ users/s0pex/home.nix
@@ -9,43 +9,47 @@
 let
   # Java SDKs
   jdksPath = ".jdks"; # IntelliJ's default path for Java SDKs
-  additionalJDKs = with pkgs; [
+  additionalJDKs = with pkgs-unstable; [
     jdk11
     jdk21
   ];
 in
 {
   # Packages
-  home.packages = with pkgs-unstable; [
-    # Dotfiles
-    chezmoi
+  home.packages =
+    with pkgs-unstable;
+    [
+      # Dotfiles
+      chezmoi
 
-    # Themes
-    nordic
+      # Themes
+      nordic
 
-    # Development tools
-    direnv
-    vscode
-    insomnia
-    gitkraken
-    # citrix_workspace
-    jetbrains.goland
-    jetbrains.datagrip
-    jetbrains.idea-ultimate
+      # Development tools
+      direnv
+      vscode
+      insomnia
+      gitkraken
+      jetbrains.goland
+      jetbrains.datagrip
+      jetbrains.idea-ultimate
 
-    # Language runtimes and SDKs
-    go
-    bun
-    nodejs_22
+      # Language runtimes and SDKs
+      go
+      bun
+      nodejs_22
 
-    # Communications
-    discord
-    ferdium
-    element-desktop
+      # Communications
+      discord
+      ferdium
+      element-desktop
 
-    # Security
-    veracrypt
-  ];
+      # Security
+      veracrypt
+    ]
+    ++ (with pkgs; [
+      citrix_workspace # Uses deprecated library and thus needs to be installed from stable
+    ]);
 
   # Java SDKs
   home.sessionPath = [ "$HOME/${jdksPath}" ];
