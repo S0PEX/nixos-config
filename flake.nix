4fcb6e1d97769860d3e6a8c9c4ad09e2670d5a34
Artur Komaristych <artur.komaristych@gmail.com>
2025-12-12 15:29:09 +0100

    refactor(flake): inline shell.nix into flake devShells

--- flake.nix
+++ flake.nix
@@ -8,28 +8,53 @@
 
     # Home Manager for user-specific configurations
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11?shallow=1";
+      url = "github:nix-community/home-manager/master?shallow=1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs =
-    inputs:
+    {
+      self,
+      nixpkgs,
+      ...
+    }@inputs:
     let
-      # NixOS system version - used to align nixpkgs and home-manager versions
-      # Please keep in sync with the version passed to nixpkgs and home-manager URLs
-      systemVersion = "25.11";
+      system = "x86_64-linux";
+      pkgs = inputs.nixpkgs.legacyPackages.${system};
 
       # Helper method to create nixosSystem with given system name and user
       mkSystem = import ./lib/mkSystem.nix {
-        inherit inputs systemVersion;
+        inherit inputs;
       };
     in
     {
+      # UM790 NixOS configuration
       nixosConfigurations.um790 = mkSystem {
         user = "s0pex";
-        system = "x86_64-linux";
+        inherit system;
         systemName = "um790";
+        systemVersion = "25.05";
+      };
+
+      # Expose nixfmt-rfc-style for 'nix fmt'
+      formatter.${system} = pkgs.nixfmt-rfc-style;
+
+      # Development shell for NixOS configuration repository
+      devShells.${system}.default = pkgs.mkShellNoCC {
+        name = "nixos-dev-shell";
+
+        # Pre-commit hooks ensure code quality before distributed version control
+        shellHook = ''
+          ${pkgs.pre-commit}/bin/pre-commit install
+        '';
+
+        packages = with pkgs; [
+          statix
+          treefmt
+          pre-commit
+          nixfmt-rfc-style
+        ];
       };
     };
 }
